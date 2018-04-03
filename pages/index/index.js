1c3b18593baf4889cb881cb219b46cbab8ec6287
arron.zhang <arron.zhang@aihuishou.com>
2018-04-03 11:49:32 +0800

    添加自定义组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -57,4 +57,23 @@ Page({
       hasUserInfo: true
     })
   },
+  onReady: function () {
+    //获得dialog组件
+    this.dialog = this.selectComponent("#dialog");
+  },
+
+  showDialog() {
+    this.dialog.showDialog();
+  },
+
+  //取消事件
+  _cancelEvent() {
+    console.log('你点击了取消');
+    this.dialog.hideDialog();
+  },
+  //确认事件
+  _confirmEvent() {
+    console.log('你点击了确定');
+    this.dialog.hideDialog();
+  }
 })
